f2dcb62b351b94810df12b61615c1468fd10bb76
unknown <joshxchris098@gmail.com>
2025-04-05 16:29:39 +0100

    working on the sign in screen

--- app/src/main/java/com/example/chattingapp/MainAppComposable.kt
+++ app/src/main/java/com/example/chattingapp/MainAppComposable.kt
@@ -4,11 +4,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.chattingapp.feature.auth.signin.SignInScreen
+import com.example.chattingapp.feature.auth.signup.SignUpScreen
 
 @Composable
 fun MainApp(){
     Surface(modifier = Modifier.fillMaxSize()) {
         val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "login"){
+            composable("login") {
+                SignInScreen(navController)
+            }
+            composable("signup") {
+                SignUpScreen(navController)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/chattingapp/feature/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/example/chattingapp/feature/auth/signin/SignInScreen.kt
@@ -1,2 +1,63 @@
 package com.example.chattingapp.feature.auth.signin
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.chattingapp.R
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SignInScreen(navController: NavController = rememberNavController()){
+    var email = remember{ mutableStateOf("") }
+    var password = remember{ mutableStateOf("")}
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(it),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier.size(80.dp)
+                    .background(
+                        Color.White
+                    )
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = {"Email"}
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = {"Password"}
+            )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    SignInScreen()
+}
